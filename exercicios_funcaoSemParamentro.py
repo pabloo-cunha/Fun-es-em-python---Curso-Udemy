da23280322d87bc85926a614979df6fe461701e6
Pablo Cunha <111435624+pabloo-cunha@users.noreply.github.com>
2022-11-28 22:58:28 -0300

    Update exercicios_funcaoSemParamentro.py

--- exercicios_funcaoSemParamentro.py
+++ exercicios_funcaoSemParamentro.py
@@ -1,3 +1,15 @@
+"""
+Exercícios:
+1) Foi realizada uma pesquisa de algumas características e gostos de quatro
+habitantes incluindo: nome, sexo, esporte favorito (Natação, Futebol, Volêi,
+Tênis) e idade. Com esses dados faça:
+- Função que armazene os dados em uma lista. Dica: Use dicionários dentro da
+lista.
+- Calcule a idade média de homens que gostam de natação. Caso não haja
+homens que gostam de natação chame uma função e imprima um aviso de que
+não há homens que gostam de natação.
+
+"""
 def cadastro():
     list = []
     for i in range(4):
